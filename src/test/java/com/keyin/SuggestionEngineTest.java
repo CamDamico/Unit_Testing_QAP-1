3d800d8750ff03d20419ed354f3d953fb4e105ab
CamDamico <camerondamico2001@gmail.com>
2023-06-04 12:57:39 -0230

    Added a testSuggestion unit test

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -1,2 +1,21 @@
-package com.keyin;public class SuggestionEngineTest {
+package com.keyin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class SuggestionEngineTest {
+
+    @Test
+    public void testSuggestion() throws IOException {
+        SuggestionEngine suggestEngine = new SuggestionEngine();
+        suggestEngine.loadDictionaryData(Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
+
+        Assertions.assertTrue(suggestEngine.generateSuggestions("heig-t").contains("height"));
+    }
+
+
+
 }
