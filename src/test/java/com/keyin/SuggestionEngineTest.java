b833ec70fd4154e550925adf53db44c6e6d7981e
CamDamico <camerondamico2001@gmail.com>
2023-06-05 19:05:12 -0230

    Added a testSuggestion unit test

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class SuggestionEngineTest {
 
     @Test
@@ -16,6 +18,23 @@ public class SuggestionEngineTest {
         Assertions.assertTrue(suggestEngine.generateSuggestions("heig-t").contains("height"));
     }
 
+    @Test
+    public void failSuggestion() throws IOException{
+        SuggestionEngine suggestEngine = new SuggestionEngine();
+        suggestEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").getPath()));
 
+        assertFalse(suggestEngine.generateSuggestions("hello").contains("hello"));
+    }
+
+    @Test
+    public void generateSuggestionIncorrectSpelling(){
+        SuggestionEngine suggestEngine = new SuggestionEngine();
+
+        String word = "Heighht";
+
+        String suggestions = suggestEngine.generateSuggestions(word);
+
+        assertFalse(suggestions.equals(word), "Suggestions shouldn't be empty for misspelled words");
+    }
 
 }
